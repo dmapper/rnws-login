6e0f66a6f095794809073c98d77fb385342c862a
ByShock <Ivan.Lepkin@yandex.ru>
2019-02-26 20:26:31 +0300

    changed entry point

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-export { default as Login } from './components/Login'
+require('./lib/passport');
+require('./lib/middleware');
+require('./lib/api');
+require('./lib/crypt');
+
+var Login = require('./Login');
+
+module.exports = new Login();
+
+export { default as LoginComponent } from './components/Login'
