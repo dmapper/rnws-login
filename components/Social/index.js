6ca19f69f51fcd119888b51191311b6f819af0df
ByShock <ivanlepkincode@gmail.com>
2019-02-27 13:46:43 +0300

    reworked styles

--- components/Social/index.js
+++ components/Social/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { TouchableOpacity, Linking } from 'react-native'
+import { TouchableOpacity, Linking, Button } from 'react-native'
 import { observer } from 'react-sharedb'
+import './index.styl'
 
 export default observer (function Social ({children, strategy, ...props}) {
   return (
-    <TouchableOpacity onPress={() => Linking.openURL(`/auth/${strategy}`)}>
-      {children}
+    <TouchableOpacity 
+      styleName='root'  
+      onPress={() => Linking.openURL(`/auth/${strategy}`)}
+    >
+      {children || <Button title={strategy}/>}
     </TouchableOpacity>
   )
 })
